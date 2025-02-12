7ed8ea091df566b7210613ed635e94fd99d793b1
Ankit jha <jhaankit499@gmail.com>
2025-02-12 23:02:09 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "./components/Auth/Auth";
 import Callback from "./components/Callback/Callback";
 import Dashboard from "./components/Dashboard/Dashboard";
